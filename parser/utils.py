25eca42f625301b85fb6bf607238317a49c22d66
Dmitriy Sataev <sataev.dima@mail.ru>
2024-06-07 19:36:41 +0300

    fix parser
    улучшен метод сохранения изображений в директорию

--- parser/utils.py
+++ parser/utils.py
@@ -1,4 +1,23 @@
 from enum import Enum
+import urllib.request
+import csv
+from datetime import datetime
+import os
 
 def enum_to_int(v: Enum):
     return v.value
+
+def save_to_directory(fileName: str):
+    now = str(datetime.now())
+    now_formatted = now.replace(':', '_')
+    # getting save dir name
+    save_dir = f'images/{now_formatted}'
+    # make save dir
+    os.makedirs(save_dir)
+    
+    with open(fileName, 'r') as csvfile:
+        filedatareader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in filedatareader:
+            url = row[1]
+            if(len(url) > 0):
+                urllib.request.urlretrieve(url, f'{save_dir}/{row[0]}.jpg')
